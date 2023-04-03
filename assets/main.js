a6efba7d3ed0a3f49b3848c0598708af2c943b31
Steven Noreña Vergel <101996576+snorena22dev@users.noreply.github.com>
2023-04-03 02:31:49 +0200

    Menús JS y estilos

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,49 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 
+const menuHamIcon = document.querySelector('.hamicon')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+
+const menuCartIcon = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu)
+menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuCartIcon.addEventListener('click', toggleCartAside)
+
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive')
-}
+
+} 
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if (!isAsideClosed){
+        aside.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive')
+
+} 
+function toggleCartAside(){
+    const isDesktopMenu = desktopMenu.classList.contains('inactive')
+    const isMobileMenu = mobileMenu.classList.contains('inactive')
+
+    if (!isDesktopMenu){
+        desktopMenu.classList.add('inactive')
+    }
+    if (!isMobileMenu){
+        mobileMenu.classList.add('inactive')
+    }
+
+    aside.classList.toggle('inactive')
+
+} 
